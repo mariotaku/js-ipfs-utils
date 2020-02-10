c34023108b4a1949b4113b11267cfb864b10ef13
Mariotaku <mariotaku.lee@gmail.com>
2020-02-10 10:43:51 +0900

    Update url-source.js

--- src/files/url-source.js
+++ src/files/url-source.js
@@ -3,10 +3,13 @@
 const { default: ky } = require('ky-universal')
 const toIterable = require('stream-to-it/source')
 
+/*
+Options are used for `ky.get`.
+*/
 module.exports = async function * urlSource (url, options) {
   options = options || {}
 
-  const { body } = await ky.get(url)
+  const { body } = await ky.get(url, options)
 
   yield {
     path: decodeURIComponent(new URL(url).pathname.split('/').pop() || ''),
